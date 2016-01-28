d421f80472b921118b298a3193a249b95ca35dcc
Brian Beggs <bbeggs@edx.org>
2016-01-28 14:04:55 -0500

    a randomly generated ami id is now returned for each run of the script

--- echo.py
+++ echo.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 import json
+import uuid
 
-build_info = {'imageId':'ami-bc223sdfd'}
+build_info = {'imageId':str(uuid.uuid4())[-9:]}
 with open('createdami.json', 'w') as outfile:
     json.dump(build_info, outfile)
 
