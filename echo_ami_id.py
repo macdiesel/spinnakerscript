bbc8b7a99b3f78b970a543759e3428c994f41766
Brian Beggs <bbeggs@edx.org>
2016-01-28 13:47:27 -0500

    addeded script to echo ami_id

--- echo_ami_id.py
+++ echo_ami_id.py
@@ -1,8 +1,19 @@
 import click
+import sys
 
 @click.command()
 @click.option('--ami_id', '-i',
               help='the AMI ID to echo',
               type=str)
 def echo_ami_id(ami_id):
-    print "AMI id passed to this script: %s".format(ami_id)
+    exit_status = True
+    if ami_id is not None:
+        print "AMI id passed to this script: {}".format(ami_id)
+    else:
+        print "No AMI id passed to the script!"
+        exit_status = False
+
+    sys.exit(not exit_status)
+
+if __name__ == '__main__':
+    echo_ami_id()
